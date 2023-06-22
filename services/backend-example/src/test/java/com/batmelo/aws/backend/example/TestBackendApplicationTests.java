0a1a594545b58c5e327b3f530d3513c55d389d0a
Vinicius Dantas e Melo <vdmelo@gmail.com>
2023-06-22 10:27:03 -0300

    Backend example service

---
+++ services/backend-example/src/test/java/com/batmelo/aws/backend/example/TestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batmelo.aws.backend.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
